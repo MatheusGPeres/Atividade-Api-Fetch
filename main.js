37993d8e9478032d26d8b2ee1235763d168de220
Matheus Graciano Peres <mgracianoperes@icloud.com>
2024-06-12 03:23:35 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+function mostraConselho(x, y) {
+    let p = document.createElement('p');
+    p.textContent = "Conselho nº " + y + ": " + x;
+    p.setAttribute("class", "conselho"); // Adiciona uma classe para referenciar mais tarde
+    document.body.appendChild(p);
+}
+
+function removerUltimoConselho() {
+    let conselhos = document.querySelectorAll(".conselho");
+    if (conselhos.length > 0) {
+        let ultimoConselho = conselhos[conselhos.length - 1];
+        ultimoConselho.parentNode.removeChild(ultimoConselho);
+    } else {
+        console.log("Não há conselhos para remover.");
+    }
+}
+
+function removerTodosConselhos() {
+    let conselhos = document.querySelectorAll(".conselho");
+    conselhos.forEach(conselho => conselho.parentNode.removeChild(conselho));
+}
+
+function fetchConselho() {
+    fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(data => mostraConselho(data.slip.advice, data.slip.id))
+        .catch(error => console.error('Erro:', error));
+}
+
+function main() {
+    document.querySelector("#btnConselho")
+        .addEventListener('click', evt => {
+            fetchConselho();
+        });
+
+    document.querySelector("#btnApagaUm")
+        .addEventListener('click', evt => {
+            removerUltimoConselho();
+        });
+
+    document.querySelector("#btnApagaTodos")
+        .addEventListener('click', evt => {
+            removerTodosConselhos();
+        });
+}
+
+window.onload = main;
